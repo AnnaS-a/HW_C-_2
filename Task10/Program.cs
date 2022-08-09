09279480397f5ea7f059eecaa091dcf0335814aa
anna <slukin1@yandex.ru>
2022-08-09 21:03:55 +0700

    tesk10

---
+++ Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine("Введите положительное трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int RemoveSecond(int num)
+{
+    int number1 = num / 100; // первая цифра
+    int number2 = num % 10; // последняя цифра
+    int nov = (num - (number1 * 100 + number2))/10;
+    return nov;
+}
+int result = RemoveSecond(number);
+
+if (number > 100 && number < 1000)
+Console.WriteLine($"{number} -> {result}");
+else
+    Console.WriteLine("Некорректный ввод числа");
+
